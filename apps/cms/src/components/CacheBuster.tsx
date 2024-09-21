d2a1908d247c499fed04ed028c3eb2047b3beddc
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-09-20 20:25:18 -0400

    chore: remove dead "Tool" page

--- apps/cms/src/components/CacheBuster.tsx
+++
@@ -1,8 +0,0 @@
-export default async function CacheBuster() {
-  return (
-    <>
-      <input type="text" placeholder="Route to cache-bust" />
-      <button type="button">Bust Cache</button>
-    </>
-  )
-}

2c0530641b63b7bab23d8d0d069310484f3411de
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-11-13 22:35:38 -0500

    fix(a11y): add alt text to image-with-text-node

--- apps/web/components/content/image-with-text-node.tsx
+++ apps/web/components/content/image-with-text-node.tsx
@@ -11,6 +11,7 @@ export function ImageWithTextNode({ image, text }: Props) {
     <div className="flex flex-wrap gap-4 mb-10 max-sm:flex-col max-sm:flex-nowrap max-sm:w-fit">
       <img
         src={image.url || ""}
+        alt={image.alt}
         className="w-[300px] h-auto aspect-square rounded-full max-sm:mx-auto"
       ></img>
       <PageContent content={text as any} className="sm:basis-[300px] grow" />

d6455f554bbdef0db814d7f6de2e4537c0532489
Greg Willard (r3pwn) <wickett06@gmail.com>
2025-02-17 18:42:37 -0500

    feat: add basic web-astro project

---
+++ apps/web-astro/astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      iconDir: '../../packages/icons/resources'
+    })
+  ],
+  output: 'server',
+  adapter: vercel(),
+});
---
+++ apps/web-astro/src/providers/content-provider.ts
@@ -0,0 +1,56 @@
+import { getPayload } from "payload";
+import config from "@local/payload/config";
+
+const payload = await getPayload({
+  config,
+});
+
+type AbridgedPageData = {
+  title: string;
+  url: string;
+  updatedAt: string;
+};
+
+export const getPageList = async () => {
+  const data = await payload.find({
+    collection: "pages",
+    select: {
+      title: true,
+      url: true,
+      updatedAt: true,
+    },
+  });
+
+  return data.docs as AbridgedPageData[];
+};
+
+export const getPageByUrl = async (url: string) => {
+  try {
+    const data = await payload.find({
+      collection: "pages",
+      limit: 1,
+      where: {
+        url: {
+          equals: url,
+        },
+      },
+      select: {
+        title: true,
+        subtitle: true,
+        breadcrumbs: true,
+        content: true,
+        meta: true,
+      },
+    });
+
+    return data.docs.at(0);
+  } catch {
+    return null;
+  }
+};
+
+export const getNavigation = async () => {
+  return await payload.findGlobal({
+    slug: "navigation",
+  });
+};

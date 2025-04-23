7efbd112e8ad1d03e064fd9e95d0542957ea4620
Greg Willard (r3pwn) <wickett06@gmail.com>
2025-04-23 19:38:21 -0400

    feat(cms): switch revalidation logic to astro-web

--- packages/payload/utils/revalidate-helper.ts
+++ packages/payload/utils/revalidate-helper.ts
@@ -1,22 +1,21 @@
-const REVALIDATE_API_PREFIX = `${process.env.SITE_HOST}/api/revalidate`
-const REVALIDATE_API_SECRET = process.env.REVALIDATE_SECRET || ''
+const REVALIDATE_API_SECRET = process.env.REVALIDATE_SECRET || "";
 
 const commonHeaders = (): HeadersInit => ({
-  'X-REVALIDATE-SECRET': REVALIDATE_API_SECRET,
-})
+  "x-prerender-revalidate": REVALIDATE_API_SECRET,
+});
 
 export const revalidatePages = async (pages: string[]) => {
-  return await fetch(`${REVALIDATE_API_PREFIX}/pages`, {
-    method: 'POST',
-    body: JSON.stringify(pages),
-    headers: commonHeaders(),
-  })
-}
+  await Promise.all(
+    pages.map((page) => {
+      return fetch(`${process.env.SITE_HOST}${page}`, {
+        method: "HEAD",
+        headers: commonHeaders(),
+      });
+    })
+  );
+};
 
 export const revalidateTags = async (tags: string[]) => {
-  return await fetch(`${REVALIDATE_API_PREFIX}/tags`, {
-    method: 'POST',
-    body: JSON.stringify(tags),
-    headers: commonHeaders(),
-  })
-}
+  console.log("TODO: Revalidate tags:", tags);
+  return;
+};

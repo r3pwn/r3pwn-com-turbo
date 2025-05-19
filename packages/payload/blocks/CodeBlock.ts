588fbfca95dbd65fc2710a323dbfa76ab86f2a12
Greg Willard (r3pwn) <wickett06@gmail.com>
2025-05-18 21:05:56 -0400

    feat(web-astro): add HTTP 500 page

--- packages/payload/blocks/CodeBlock.ts
+++ packages/payload/blocks/CodeBlock.ts
@@ -9,6 +9,7 @@ export const CodeBlock: Block = {
       type: "select",
       required: true,
       defaultValue: "plain",
+      // Check https://prismjs.com/#supported-languages for supported languages
       options: [
         {
           label: "Plaintext",
